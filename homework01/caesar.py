7df066750b14f602392b2502c88c2e268d9fe8da
JuliaIrzhanova <julirzhanova@mail.ru>
2023-11-14 16:51:13 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -14,43 +14,11 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     for ch in plaintext:
         if ch.isalpha():
             code = ord(ch) + shift
-            if ch in 'XYZxyz':
-                code-=26
+            if ch in "XYZxyz":
+                code -= 26
             finalLetter = chr(code)
             ciphertext += finalLetter
         else:
-            ciphertext+=ch
-
+            ciphertext += ch
 
     return ciphertext
-
-
-for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
-    print(i, ord(i))
-
-
-def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    for ch in ciphertext:
-        if ch.isalpha():
-            code = ord(ch) - shift
-            if ch in 'ABCabc':
-                code+=26
-            finalLetter = chr(code)
-            plaintext += finalLetter
-        else:
-            plaintext+=ch
-
-    return plaintext 
-
