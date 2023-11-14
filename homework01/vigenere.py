08d7e99724fb91733e097a627bf367a4acc38913
JuliaIrzhanova <julirzhanova@mail.ru>
2023-11-14 17:09:03 +0300

    Реализована функция decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -32,35 +32,35 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     return ciphertext
 
 
-# def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-#     """
-#     Decrypts a ciphertext using a Vigenere cipher.
-#     >>> decrypt_vigenere("PYTHON", "A")
-#     'PYTHON'
-#     >>> decrypt_vigenere("python", "a")
-#     'python'
-#     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-#     'ATTACKATDAWN'
-#     """
-#     plaintext = ""
-#     a = 0
-#     while len(keyword) < len(ciphertext):
-#         keyword += keyword[a]
-#         a += 1
-#     for i in range(len(keyword)):
-#         if keyword[i].isupper():
-#             key = ord(keyword[i]) - 65
-#         elif keyword[i].islower():
-#             key = ord(keyword[i]) - 97
-#         if ciphertext[i].isalpha():
-#             c = ord(ciphertext[i])
-#             if ciphertext[i].isupper() and c <= 64 + key:
-#                 plaintext += chr(c + 26 - key)
-#             elif ciphertext[i].islower() and c <= 96 + key:
-#                 plaintext += chr(c + 26 - key)
-#             else:
-#                 plaintext += chr(c - key)
-#         else:
-#             plaintext += ciphertext[i]
-#
-#     return plaintext
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    plaintext = ""
+    a = 0
+    while len(keyword) < len(ciphertext):
+        keyword += keyword[a]
+        a += 1
+    for i in range(len(keyword)):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97
+        if ciphertext[i].isalpha():
+            c = ord(ciphertext[i])
+            if ciphertext[i].isupper() and c <= 64 + key:
+                plaintext += chr(c + 26 - key)
+            elif ciphertext[i].islower() and c <= 96 + key:
+                plaintext += chr(c + 26 - key)
+            else:
+                plaintext += chr(c - key)
+        else:
+            plaintext += ciphertext[i]
+
+    return plaintext
