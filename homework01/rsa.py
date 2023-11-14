47b7bf466876d71a0891b95710b66176dfbc4ee5
JuliaIrzhanova <julirzhanova@mail.ru>
2023-11-14 17:26:48 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,7 +13,7 @@ def is_prime(n: int) -> bool:
     False
     """
     k = 0
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             k += 1
     if k == 0:
