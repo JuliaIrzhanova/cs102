574e0e4b5eb2cc320cb3230633ed76f4cf87b3a7
JuliaIrzhanova <julirzhanova@mail.ru>
2024-06-04 15:26:44 +0300

    добавлены решения опять2

--- homework06/db.py
+++ homework06/db.py
@@ -1,4 +1,4 @@
-#type: ignore
+# type: ignore
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
